7aa6c624e9a20dc957cda3cb9c83a62c719817ba
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-15 20:00:48 -0300

    Mostrar ordem da palavra

---
+++ src/principal/Programa.java
@@ -0,0 +1,31 @@
+package principal;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite uma frase : ");
+		
+		try {
+			String[] vetor = sc.nextLine().split(" ");
+			System.out.println("Digite a ordem da palavra : ");
+			int posicao = sc.nextInt();
+			System.out.println(vetor[posicao]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+		  System.out.println("Posição inválida");
+		}
+		catch(InputMismatchException e) {
+		  System.out.println("Erro de entrada");
+		}
+		
+		System.out.println("Fim do Programa");
+        sc.close();
+	}
+
+}
